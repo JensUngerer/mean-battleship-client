6dffd0780ed1a7a0194cccf07ed56b622054c54a
Jens Ungerer <github@jens-ungerer.de>
2019-05-05 17:47:56 +0200

    Adding simple routes for router. Simplifying app.component.html.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameStartComponent } from './view/game-start/game-start.component';
+import { GameComponent } from './view/game/game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'battleship-game-start', pathMatch: 'full'},
+  {path: 'battleship-game-start', component: GameStartComponent},
+  {path: 'battleship-game', component: GameComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
