bb3f1ed8e92370f6dbcb808efc6cffe89b877fc2
Jens Ungerer <github@jens-ungerer.de>
2019-05-05 18:44:38 +0200

    Realizing styling via @HostBinding (on the component itself!).

--- src/app/view/field/field.component.ts
+++ src/app/view/field/field.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output } from '@angular/core';
+import { Component, OnInit, Output, HostBinding, Input } from '@angular/core';
 
 @Component({
   selector: 'bs-field',
@@ -8,16 +8,25 @@ import { Component, OnInit, Output } from '@angular/core';
 export class FieldComponent implements OnInit {
   @Output()
   public caption: string = 'TODO: Title of field';
-  
+
   @Output()
   public legendTiles: [] = [];
 
   @Output()
   public tiles: [][] = [];
 
+  @Input()
+  public isDomesticField: boolean;
+
+  @HostBinding('class.fieldContainer') private isFieldContainerClass = true;
+  @HostBinding('class.domesticField') private isDomesticFieldClass = false;
+  @HostBinding('class.adversarialField') private isAdversarialFieldClass = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.isDomesticFieldClass = this.isDomesticField;
+    this.isAdversarialFieldClass = !this.isDomesticField;
   }
 
 }
--- src/app/view/game/game.component.ts
+++ src/app/view/game/game.component.ts
@@ -3,7 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'bs-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: [
+    './game.component.css',
+    './../css/domestic-field.css',
+    './../css/adversarial-field.css'
+  ]
 })
 export class GameComponent implements OnInit {
 
