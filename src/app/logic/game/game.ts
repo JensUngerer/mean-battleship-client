d7712bbb23e3479c96d9bfe0b6789a69eeb1ecd1
Jens Ungerer <github@jens-ungerer.de>
2019-05-14 02:08:15 +0200

    adding logic to change the tile state (domestic). (only for debugging purposes; not yet with communication-service)

--- src/app/logic/game/game.ts
+++ src/app/logic/game/game.ts
@@ -1,5 +1,6 @@
 import { Tile } from '../tile/tile';
 import { ITileCoordinates } from '../../../../../common/src/tileCoordinates/iTileCoordinates';
+import { TileState } from '../../../../../common/src/tileState/tileState.enum';
 
 export class Game {
   public domesticTiles: Tile[][];
@@ -12,6 +13,44 @@ export class Game {
   }
 
   public onFired: (coordinates: ITileCoordinates) => void = (coordinates: ITileCoordinates) => {
+    // DEBUGGING:
+    // console.log(coordinates);
+
+    // only for DEBUGGING purposes
+    this.receiveCoordinates(coordinates);
+
+
+    // this.setDomesticTileState(coordinates);
+    this.sendCoordinates(coordinates);
+
+    // TODO: as a kind of response the adversarial tile-state has to be set...
+    // this.setAdversarialTileState()
+  }
+
+  private sendCoordinates(coordinates: ITileCoordinates){
+    // DEBUGGING:
     console.log(coordinates);
-  };
+  }
+
+  public receiveCoordinates(coordinates: ITileCoordinates) {
+    this.setDomesticTileState(coordinates);
+  }
+
+
+  public setDomesticTileState(coordinates: ITileCoordinates) {
+    const domesticTile = this.domesticTiles[coordinates.rowIndex][coordinates.columnIndex];
+    const domesticTileState = domesticTile.tileState;
+    let newDomesticTileState;
+    if (domesticTileState === TileState.Water) {
+      newDomesticTileState = TileState.WaterFired;
+    }
+    if (domesticTileState === TileState.Ship) {
+      newDomesticTileState = TileState.ShipFired;
+    }
+    if (domesticTileState === TileState.ShipFired) {
+      newDomesticTileState = TileState.ShipSunken;
+    }
+    domesticTile.tileState = newDomesticTileState;
+  }
+
 }
