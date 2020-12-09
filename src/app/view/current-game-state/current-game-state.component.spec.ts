fff2a64ad61cfdc624731b84bd3dc7e16e20d984
Jens Ungerer <github@jens-ungerer.de>
2020-12-09 10:27:31 +0100

    first run of ng update

--- src/app/view/current-game-state/current-game-state.component.spec.ts
+++ src/app/view/current-game-state/current-game-state.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { CurrentGameStateComponent } from './current-game-state.component';
 
@@ -6,7 +6,7 @@ describe('CurrentGameStateComponent', () => {
   let component: CurrentGameStateComponent;
   let fixture: ComponentFixture<CurrentGameStateComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ CurrentGameStateComponent ]
     })
--- src/app/view/legend-tile-corner/legend-tile-corner.component.spec.ts
+++ src/app/view/legend-tile-corner/legend-tile-corner.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { LegendTileCornerComponent } from './legend-tile-corner.component';
 
@@ -6,7 +6,7 @@ describe('LegendTileCornerComponent', () => {
   let component: LegendTileCornerComponent;
   let fixture: ComponentFixture<LegendTileCornerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ LegendTileCornerComponent ]
     })
